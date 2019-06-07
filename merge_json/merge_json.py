c0b3d68814ba5a19a570b50bee252571bdc36dfd
Thomas Maschler <tmaschler@wri.org>
2019-06-07 09:49:56 -0400

    retry on timeout
    Signed-off-by: Thomas Maschler <tmaschler@wri.org>

--- merge_json/merge_json.py
+++ merge_json/merge_json.py
@@ -1,47 +1,55 @@
+from retrying import retry
 import os
 import json
 import click
 import boto3
+import botocore
+import urllib3
 
 
-def cli():
-    code = ["IDN.{}".format(i) for i in range(1, 34)]
-    letter = "IDN"
-
-    bucket = "gfw-files"
-    s3_folder = "2018_update/results/20190425/"
+@click.command()
+@click.argument("bucket")
+@click.argument("folder")
+def cli(bucket, folder):
     datasets = ["iso", "adm1", "adm2"]
 
     for dataset in datasets:
         part = 0
         data = list()
-        for c in code:
-            prefix = os.path.join(s3_folder, c, "api", dataset)
-            click.echo("Searching: " + prefix)
 
-            for obj in _get_s3_records(bucket, prefix):
+        prefix = os.path.join(folder, "api", dataset)
+        click.echo("Searching: " + prefix)
 
-                filename, file_extension = os.path.splitext(obj.key)
-                if file_extension == ".txt":
-                    click.echo("Download: " + os.path.basename(obj.key))
-                    data += json.loads(obj.get()["Body"].read().decode("utf-8"))
-                    if len(data) > 100000:
-                        file_name = "{}-{}-part-{}.json".format(
-                            letter, dataset, str(part).zfill(4)
-                        )
-                        with open(file_name, "w") as outfile:
-                            json.dump(data, outfile)
-                        _upload_file(
-                            file_name, bucket, os.path.join(s3_folder, dataset)
-                        )
+        for obj in _get_s3_records(bucket, prefix):
 
-                        data = list()
-                        part += 1
+            filename, file_extension = os.path.splitext(obj.key)
+            if file_extension == ".txt":
+                click.echo("Download: " + os.path.basename(obj.key))
+                data += json.loads(obj.get()["Body"].read().decode("utf-8"))
+                l = len(data)
+                if l > 200000:
+                    file_name = "{}-part-{}.json".format(
+                        dataset, str(part).zfill(4)
+                    )
+                    click.echo("Write file {} with {} rows".format(file_name, l))
+                    with open(file_name, "w") as outfile:
+                        json.dump(data, outfile)
+                    _upload_file(
+                        file_name, bucket, os.path.join(folder, dataset)
+                    )
 
-        file_name = "{}-{}-part-{}.json".format(letter, dataset, str(part).zfill(4))
+                    data = list()
+                    part += 1
+
+        file_name = "{}-part-{}.json".format(dataset, str(part).zfill(4))
         with open(file_name, "w") as outfile:
             json.dump(data, outfile)
-        _upload_file(file_name, bucket, os.path.join(s3_folder, dataset))
+        _upload_file(file_name, bucket, os.path.join(folder, dataset))
+
+
+def retry_if_timeout(exception):
+    """Return True if we should retry (in this case when it's an IOError), False otherwise"""
+    return isinstance(exception, (botocore.exceptions.ReadTimeoutError, urllib3.exceptions.ReadTimeoutError))
 
 
 def _get_s3_records(bucket_name, prefix):
@@ -51,6 +59,7 @@ def _get_s3_records(bucket_name, prefix):
     return bucket.objects.filter(Prefix=prefix)
 
 
+@retry(retry_on_exception=retry_if_timeout, wait_fixed=2000)
 def _upload_file(file_name, bucket, prefix):
     s3 = boto3.resource("s3")
     s3_key = os.path.join(prefix, file_name)
